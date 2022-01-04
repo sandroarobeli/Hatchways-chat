b22224e07864f9c0cad9b7812fa507f1b2b7ad43
Alex Arobelidze <sandroarobeli77@gmail.com>
2022-01-03 20:04:42 -0600

    closes #1

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,68 +9,68 @@ import { logout, fetchConversations } from "../store/utils/thunkCreators";
 import { clearOnLogout } from "../store/index";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    height: "100vh"
-  }
+	root: {
+		height: "100vh",
+	},
 }));
 
 const Home = (props) => {
-  const classes = useStyles();
-  const { user, logout, fetchConversations } = props;
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const classes = useStyles();
+	const { user, logout, fetchConversations } = props;
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    if (user.id) {
-      setIsLoggedIn(true);
-    }
-  }, [user.id]);
+	useEffect(() => {
+		if (user.id) {
+			setIsLoggedIn(true);
+		}
+	}, [user.id]);
 
-  useEffect(() => {
-    fetchConversations();
-  }, [fetchConversations]);
+	useEffect(() => {
+		fetchConversations();
+	}, [fetchConversations]);
 
-  if (!user.id) {
-    // If we were previously logged in, redirect to login instead of register
-    if (isLoggedIn) return <Redirect to="/login" />;
-    return <Redirect to="/register" />;
-  }
+	if (!user.id) {
+		// If we were previously logged in, redirect to login instead of register
+		if (isLoggedIn) return <Redirect to="/login" />;
+		return <Redirect to="/register" />;
+	}
 
-  const handleLogout = async () => {
-    await logout(user.id);
-  };
+	const handleLogout = async () => {
+		await logout(user.id);
+	};
 
-  return (
-    <>
-      {/* logout button will eventually be in a dropdown next to username */}
-      <Button className={classes.logout} onClick={handleLogout}>
-        Logout
-      </Button>
-      <Grid container component="main" className={classes.root}>
-        <CssBaseline />
-        <SidebarContainer />
-        <ActiveChat />
-      </Grid>
-    </>
-  );
+	return (
+		<React.Fragment>
+			{/* logout button will eventually be in a dropdown next to username */}
+			<Button className={classes.logout} onClick={handleLogout}>
+				Logout
+			</Button>
+			<Grid container component="main" className={classes.root}>
+				<CssBaseline />
+				<SidebarContainer />
+				<ActiveChat />
+			</Grid>
+		</React.Fragment>
+	);
 };
 
 const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-    conversations: state.conversations
-  };
+	return {
+		user: state.user,
+		conversations: state.conversations,
+	};
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    logout: (id) => {
-      dispatch(logout(id));
-      dispatch(clearOnLogout());
-    },
-    fetchConversations: () => {
-      dispatch(fetchConversations());
-    }
-  };
+	return {
+		logout: (id) => {
+			dispatch(logout(id));
+			dispatch(clearOnLogout());
+		},
+		fetchConversations: () => {
+			dispatch(fetchConversations());
+		},
+	};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,25 @@
 const Sequelize = require("sequelize");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+	process.env.DATABASE_URL ||
+		`postgres://postgres:${process.env.SESSION_SECRET}@127.0.0.1:5432/messenger`,
+	{
+		logging: false,
+	}
+);
+
+db.authenticate()
+	.then((result) => {
+		console.log(
+			`Connection to ${process.env.DB_NAME.toUpperCase()} for ${
+				process.env.DB_USER
+			} has been established`
+		);
+	})
+	.catch((error) => {
+		console.log("Unable to connect to the database:", error);
+	});
 
 module.exports = db;
