2e82961a686d098b85a3ec9269de800cd209a9c1
Alex Arobelidze <sandroarobeli77@gmail.com>
2022-01-07 14:53:07 -0600

    style: updates formatting to be inline with original codebase

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -5,62 +5,61 @@ import { Input, Header, Messages } from "./index";
 import { connect } from "react-redux";
 
 const useStyles = makeStyles(() => ({
-	root: {
-		display: "flex",
-		flexGrow: 8,
-		flexDirection: "column",
-	},
-	chatContainer: {
-		marginLeft: 41,
-		marginRight: 41,
-		display: "flex",
-		flexDirection: "column",
-		flexGrow: 1,
-		justifyContent: "space-between",
-	},
+  root: {
+    display: "flex",
+    flexGrow: 8,
+    flexDirection: "column",
+  },
+  chatContainer: {
+    marginLeft: 41,
+    marginRight: 41,
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: 1,
+    justifyContent: "space-between",
+  },
 }));
 
 const ActiveChat = (props) => {
-	const classes = useStyles();
-	const { user } = props;
-	const conversation = props.conversation || {};
+  const classes = useStyles();
+  const { user } = props;
+  const conversation = props.conversation || {};
 
-	return (
-		<Box className={classes.root}>
-			{conversation.otherUser && (
-				<React.Fragment>
-					<Header
-						username={conversation.otherUser.username}
-						online={conversation.otherUser.online || false}
-					/>
-					<Box className={classes.chatContainer}>
-						<Messages
-							messages={conversation.messages}
-							otherUser={conversation.otherUser}
-							userId={user.id}
-						/>
-						<Input
-							otherUser={conversation.otherUser}
-							conversationId={conversation.id}
-							user={user}
-						/>
-					</Box>
-				</React.Fragment>
-			)}
-		</Box>
-	);
+  return (
+    <Box className={classes.root}>
+      {conversation.otherUser && (
+        <React.Fragment>
+          <Header
+            username={conversation.otherUser.username}
+            online={conversation.otherUser.online || false}
+          />
+          <Box className={classes.chatContainer}>
+            <Messages
+              messages={conversation.messages}
+              otherUser={conversation.otherUser}
+              userId={user.id}
+            />
+            <Input
+              otherUser={conversation.otherUser}
+              conversationId={conversation.id}
+              user={user}
+            />
+          </Box>
+        </React.Fragment>
+      )}
+    </Box>
+  );
 };
 
 const mapStateToProps = (state) => {
-	return {
-		user: state.user,
-		conversation:
-			state.conversations &&
-			state.conversations.find(
-				(conversation) =>
-					conversation.otherUser.username === state.activeConversation
-			),
-	};
+  return {
+    user: state.user,
+    conversation:
+      state.conversations &&
+      state.conversations.find(
+        (conversation) => conversation.otherUser.username === state.activeConversation
+      ),
+  };
 };
 
 export default connect(mapStateToProps, null)(ActiveChat);
--- server/db/db.js
+++ server/db/db.js
@@ -3,11 +3,11 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const db = new Sequelize(
-	process.env.DATABASE_URL ||
-		`postgres://postgres:${process.env.SESSION_SECRET}@127.0.0.1:5432/messenger`,
-	{
-		logging: false,
-	}
+  process.env.DATABASE_URL ||
+    `postgres://postgres:${process.env.SESSION_SECRET}@127.0.0.1:5432/messenger`,
+  {
+    logging: false,
+  }
 );
 
 module.exports = db;
